2ba7d061c8973a8c7301e9839a5ceb77958ea69a
Jessica Pinheiro <jessica.pinheiro@codeminer42.com>
2022-06-21 14:50:16 -0300

    Mock service worker setup

--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,28 +1,49 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen, waitForElementToBeRemoved } from '@testing-library/react';
 import SummaryForm from '../SummaryForm';
+import userEvent from '@testing-library/user-event';
 
 test('checkbox is unchecked and button is disabled by default', () => {
   render(<SummaryForm />);
-  const checkbox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' });
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
   expect(checkbox).not.toBeChecked();
 
-  const confirmButton = screen.getByRole('button', { name: 'Confirm order' });
+  const confirmButton = screen.getByRole('button', { name: /confirm order/i });
   expect(confirmButton).toBeDisabled();
 });
 
-test('checking checkbox enables button', () => {
+test('checkbox enables button on first click and disables on second click', () => {
   render(<SummaryForm />);
-  const checkbox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' });
-  const confirmButton = screen.getByRole('button', { name: 'Confirm order' });
-  fireEvent.click(checkbox);
+  const checkbox = screen.getByRole('checkbox', {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole('button', { name: /confirm order/i });
+
+  userEvent.click(checkbox);
   expect(confirmButton).toBeEnabled();
+
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
 });
 
-test('unchecking checkbox again disables button', () => {
+test('popover responds to hover', async () => {
   render(<SummaryForm />);
-  const checkbox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' });
-  const confirmButton = screen.getByRole('button', { name: 'Confirm order' });
-  fireEvent.click(checkbox);
-  fireEvent.click(checkbox);
-  expect(confirmButton).toBeDisabled();
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(/no ice cream will actually be delivered/i);
+  expect(nullPopover).not.toBeInTheDocument();
+  
+  // popover appears upon mouseover of checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+  
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popover).toBeInTheDocument();
+  
+  // popover disappears upon mouseout of checkbox label
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
